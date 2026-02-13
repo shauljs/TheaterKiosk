8b5f2d2848278b82a800a70732f949822e747670
shauljs <shauljs@mail.uc.edu>
2026-02-13 10:05:37 -0500

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args)
+    {
+        // Declarations
+        int age = 0;
+        final int DRINKING_AGE = 21;
+
+        // Create a scanner object
+        Scanner in = new Scanner(System.in);
+
+        //Prompt user input
+        System.out.println("Enter your age here:");
+
+        //Accept input from user
+
+        age = in.nextInt();
+
+        if (age >= DRINKING_AGE)
+        {
+            System.out.println("You get a wrist band. Please drink responsibly!");
         }
     }
 }
